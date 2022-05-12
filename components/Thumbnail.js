3749d8cff6fa5f202aa965ee87d4d4756b96bdc3
ethayil <ethayil@gmail.com>
2022-05-13 02:18:24 +0530

    master

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,6 +4,7 @@ import { forwardRef } from "react"
 
 const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'
+    Thumbnail.displayName = 'Thumbnail'
     return (
         <div ref={ref} className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105">
             <Image src={`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
