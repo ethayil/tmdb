2ad804f9cacb08dbfde0e5572c0a5a8baf49becc
ethayil <ethayil@gmail.com>
2022-05-13 02:03:45 +0530

    master

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images:{
+    domains:['links.papareact.com', 'this.images.tmdb.org', 'image.tmdb.org']
+  },
+}
+
+module.exports = nextConfig
